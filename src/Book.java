c759f044817123f8cc2a0d7dfc80b4a5adc067d5
Iopi4 <relentler@gmail.com>
2026-05-14 09:29:35 +0500

    Task_2

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,11 @@
+
 public class Book {
     public String title;
     public int releaseYear;
-    public String author;
+    public Author author;
     public int pages;
 
-    public Book(String title, int releaseYear, String author, int pages) {
+    public Book(String title, int releaseYear, Author author, int pages) {
         this.title = title;
         this.releaseYear = releaseYear;
         this.author = author;
@@ -20,20 +21,23 @@ public class Book {
 
     //Метод возвращающий ответ на вопрос, содержится ли в описании книги (названии или в имени автора) переданное параметром слово.
     public String matches(String word) {
-        if (title.contains(word) || author.contains(word)) {
+        if (title.contains(word) || author.name.contains(word) || author.surname.contains(word)) {
             return "-Да содержится";
         } else return "-Нет не содержится";
     }
 
     //Метод возвращающий оценку стоимости книги (целое число в рублях) по формуле 3 рубля за страницу, но не меньше 250 рублей.
     public int estimatePrice() {
-        if ((pages * 3) > 250) {
-            return pages * 3;
-        } else return 250;
+        int price = pages * 3;
+        return Math.max(price, 250);
     }
 
     //Дополнительный метод для вывода общей информации по книге и ответы на вопросы.
     public void informationBook(String word) {
-         System.out.println("[Название книги: " + title + "] " + " (Книга большая?: " + isBig() + ") " + " {Поиск введенного слова: " + matches(word) + "} " + " Стоимость книги: " + estimatePrice() + " руб.");
+        System.out.println("[Название книги: " + title + "."
+                + " Aвтор - " + author
+                + " (Книга большая?: " + isBig() + ") "
+                + " {Поиск введенного слова: " + matches(word) + "} "
+                + " Стоимость книги: " + estimatePrice() + " руб.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        //Создадим 4-х авторов для четырех книг
+        Author author1 = new Author("Джордж", "Оруэлл", 5);
+        Author author2 = new Author("Лев", "Толстой", 4);
+        Author author3 = new Author("Николай", "Гоголь", 3);
+        Author author4 = new Author("Алексей", "Толстой", 2);
+
         //Создадим четыре книги
-        Book book1 = new Book("1984", 1949, "Джордж Оруэлл", 320);
-        Book book2 = new Book("Война и Мир", 1873, "Лев Николаевич Толстой", 1500);
-        Book book3 = new Book("Мертвые души", 1842, "Николай Васильевич Гоголь", 352);
-        Book book4 = new Book("Колобок", 1965, "Алексей Николаевич Толстой", 32);
+        Book book1 = new Book("1984", 1949, author1, 320);
+        Book book2 = new Book("Война и Мир", 1873, author2, 1500);
+        Book book3 = new Book("Мертвые души", 1842, author3, 352);
+        Book book4 = new Book("Колобок", 1965, author4, 32);
 
         //Вызовем методы для отображения информации о каждой книге для пользователя с ответом на поисковое слово.
         book1.informationBook("123");
